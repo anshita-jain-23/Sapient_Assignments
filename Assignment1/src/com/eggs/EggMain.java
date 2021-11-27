c48ee3268805ffc5d146de08b1bb4c48e034fce0
anshita-jain-23 <91240441+anshita-jain-23@users.noreply.github.com>
2021-11-27 20:46:25 +0530

    Add files via upload

---
+++ Assignment1/src/com/eggs/EggMain.java
@@ -0,0 +1,12 @@
+package com.eggs;
+
+public class EggMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        CalculateEgg calc= new CalculateEgg();
+        calc.setTotalNoOfEggs(200);
+        calc.calculateEggs();
+	}
+
+}
---
+++ Assignment1/src/com/eggs/EggUnit.java
@@ -0,0 +1,25 @@
+package com.eggs;
+
+public class EggUnit {
+	int gross;
+	int dozens;
+	int remaining;
+	public int getGross() {
+		return gross;
+	}
+	public void setGross(int gross) {
+		this.gross = gross;
+	}
+	public int getDozens() {
+		return dozens;
+	}
+	public void setDozens(int dozens) {
+		this.dozens = dozens;
+	}
+	public int getRemaining() {
+		return remaining;
+	}
+	public void setRemaining(int remaining) {
+		this.remaining = remaining;
+	}
+}
